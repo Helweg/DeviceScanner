8187dbe4618435407882ee552c2a4acfdfec5874
Helweg <kenneth@helweghansen.dk>
2017-05-17 21:26:47 +0200

    Update passiveScan.py

--- opt/scanner/passiveScan.py
+++ opt/scanner/passiveScan.py
@@ -1,11 +1,1 @@
-import nmap
-nm = nmap.PortScanner()
-nm.scan(hosts='192.168.1.0/28', arguments='-sP -n')
-for h in nm.all_hosts():
-	if 'mac' in nm[h]['addresses']:
-		scanR = (nm[h]['addresses'])
 
-		if (str(scanR)+'\n') not in open('deviceList.txt').read():
-			print ("true")
-		else:
-			print ("false")
