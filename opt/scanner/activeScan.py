4009e786982be440d2cef243b64459f2ee2190f7
Helweg <kenneth@helweghansen.dk>
2017-05-18 11:09:47 +0200

    Update activeScan.py

--- opt/scanner/activeScan.py
+++ opt/scanner/activeScan.py
@@ -1,13 +1,13 @@
 import nmap
 nm = nmap.PortScanner()
-o=open('/opt/devices/Output.txt','w+')
+o=open('/opt/scanner/opt/devices/Output.txt','w+')
 nm.scan(hosts='192.168.80.0/24', arguments='-sP -n')
 for h in nm.all_hosts():
 	o.write(h+'\n')
 	if 'mac' in nm[h]['addresses']:
 		scanR = (nm[h]['addresses'])
 
-		if (str(scanR)+'\n') not in open('/opt/devices/allowedDevices.txt').read():
+		if (str(scanR)+'\n') not in open('/opt/scanner/opt/devices/allowedDevices.txt').read():
 			o.write(str(scanR)+'\n')
 			print ("true")
 		else:
