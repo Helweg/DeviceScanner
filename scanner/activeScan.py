63cf99541c4c8442457d723fcc57a9f9cf0bad8b
Helweg <kenneth@helweghansen.dk>
2017-05-19 20:13:57 +0200

    Update activeScan.py

--- scanner/activeScan.py
+++ scanner/activeScan.py
@@ -16,15 +16,16 @@ for doc in docs:
 file = open('/opt/deviceScanner/devices/allowedDevices.txt', 'a+')
 for devices in allowedDevices:
 	file.write(str(devices)+'\n')
-
-nm = nmap.PortScanner()
-o=open('/opt/deviceScanner/devices/Output.txt','a+')
-nm.scan(hosts = subnet, arguments = '-sP -n')
-for h in nm.all_hosts():
-	if 'mac' in nm[h]['addresses']:
-		scanR = (nm[h]['addresses'])
-
-		if (str(scanR)+'\n') not in file:
-			o.write(str(scanR)+' Not in allowedDevices.txt\n')
-		else:
-			o.write(str(scanR)+'\n')
+while True:
+	nm = nmap.PortScanner()
+	o=open('/opt/deviceScanner/devices/Output.txt','a+')
+	nm.scan(hosts = subnet, arguments = '-sP -n')
+	for h in nm.all_hosts():
+		if 'mac' in nm[h]['addresses']:
+			scanR = (nm[h]['addresses'])
+	
+			if (str(scanR)+'\n') not in file:
+				o.write(str(scanR)+' Not in allowedDevices.txt\n')
+			else:
+				o.write(str(scanR)+'\n')
+	time.sleep(interval)
