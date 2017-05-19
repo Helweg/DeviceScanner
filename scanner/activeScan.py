1d51c30b180cb696eb8b8ce602f2ffb8c3297b2f
Helweg <kenneth@helweghansen.dk>
2017-05-19 17:50:00 +0200

    Update activeScan.py

--- scanner/activeScan.py
+++ scanner/activeScan.py
@@ -1,14 +1,30 @@
 import nmap
 import time
+import yaml
+
+stream = open("scannerConf.yml", 'r')
+docs = yaml.load_all(stream)
+for doc in docs:
+	for k,v in doc.items():
+		if k == 'Allowed Devices':
+			allowedDevices = v
+		if k == 'Subnet':
+			subnet = v
+		if k == 'Interval':
+			interval = v
+
+file = open('/opt/deviceScanner/devices/allowedDevices.txt', 'a+')
+for devices in allowedDevices:
+	file.write(str(devices)+'\n')
 
 nm = nmap.PortScanner()
 o=open('/opt/deviceScanner/devices/Output.txt','a+')
-nm.scan(hosts='192.168.24.0/24', arguments='-sP -n')
+nm.scan(hosts = subnet, arguments = '-sP -n')
 for h in nm.all_hosts():
 	if 'mac' in nm[h]['addresses']:
 		scanR = (nm[h]['addresses'])
 
-		if (str(scanR)+'\n') not in open('/opt/deviceScanner/devices/allowedDevices.txt').read():
+		if (str(scanR)+'\n') not in file:
 			o.write(str(scanR)+' Not in allowedDevices.txt\n')
 		else:
 			o.write(str(scanR)+'\n')
